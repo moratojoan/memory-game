8803925f329a174b97d9d2eeaf4fee30505b903e
Joan Morató <moratojoan@gmail.com>
2021-06-26 15:07:49 +0200

    grid container break-point 992px

--- src/Cards.js
+++ src/Cards.js
@@ -6,7 +6,7 @@ export default function Cards({cards}) {
   return (
     <div className="container">
       {cards.map(({id, ...props}) => (
-        <div  key={id}>
+        <div key={id}>
           <Card
             {...props}
           />
