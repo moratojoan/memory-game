467d63cef0fbdf0b6e589e721be8296a05988223
Joan Morató <moratojoan@gmail.com>
2021-06-28 13:04:35 +0200

    refactor: getCards service

--- src/customHooks/useCards.js
+++ src/customHooks/useCards.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
-import shuffleArray from '../utils/shuffleArray';
-import generateUid from '../utils/generateUid';
+import { getCards } from "../services/getCards";
 
 
 export function useCards(images) {
@@ -82,38 +81,5 @@ export function useCards(images) {
             unselectAllCards
         }
     };
-}
-
-function getCards(images) {
-    const numberOfImagesToSelect = images.length > 6 ? 6 : images.length;
-
-    let selectedImageIndex = [];
-    while(selectedImageIndex.length < numberOfImagesToSelect) {
-        const randomImageIndex = Math.floor(Math.random() * images.length);
-        if(!selectedImageIndex.includes(randomImageIndex)) {
-            selectedImageIndex.push(randomImageIndex);
-        }
-    }
-
-    const singleCards = selectedImageIndex.map(imageIndex => ({
-        id: generateUid(),
-        imageId: images[imageIndex].id,
-        url: images[imageIndex].url,
-        alt: images[imageIndex].alt,
-        selected: false,
-        discovered: false
-    }));
-
-    const duplicatedCards = singleCards.flatMap(card => (
-        [
-            card,
-            {
-                ...card,
-                id: generateUid()
-            }
-        ]
-    ));
-
-    return shuffleArray(duplicatedCards);
 }
 
